f9028ed2f9c4b3939b4aa3b870a412ee49b53e0f
jdmiguel <jaimedmiguel@gmail.com>
2020-06-30 19:31:35 +0100

    add footer component and unit test

---
+++ components/Footer/__tests__/footer.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Footer from '../Footer';
+
+import { renderWithTheme } from '../../../helpers/Theme';
+
+describe('Component: Footer', () => {
+  it('should render', () => {
+    const { container } = render(renderWithTheme(<Footer />));
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
