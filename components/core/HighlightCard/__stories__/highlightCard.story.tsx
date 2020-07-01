1bd30d971432e8cfac41a6a3c05bc22abe577d73
jdmiguel <jaimedmiguel@gmail.com>
2020-07-01 18:05:59 +0100

    add component highlightCard, story and unit test

---
+++ components/core/HighlightCard/__stories__/highlightCard.story.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { text } from '@storybook/addon-knobs';
+
+import HighlightCard from '../HighlightCard';
+
+import { HIGHLIGHT_CARD_PROPS_MOCKS } from '../__mocks__/highlightCard.mocks';
+
+const stories = storiesOf('HighlightCard', module);
+
+stories.add('HighlightCard', () => (
+  <HighlightCard
+    {...HIGHLIGHT_CARD_PROPS_MOCKS}
+    title={text('title', HIGHLIGHT_CARD_PROPS_MOCKS.title)}
+    description={text('description', HIGHLIGHT_CARD_PROPS_MOCKS.description)}
+    linkText={text('link-text', HIGHLIGHT_CARD_PROPS_MOCKS.linkText)}
+  />
+));
