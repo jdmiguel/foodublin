c32fbc0e1afc0665d61c39ede2535ec15626312b
jdmiguel <jaimedmiguel@gmail.com>
2020-07-01 17:17:12 +0100

    add finder component and unit test

---
+++ components/Finder/Finder.tsx
@@ -0,0 +1,177 @@
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+
+import { CDN_URL_STATIC_DIRECTORY } from '../../helpers/utils';
+
+import Autocomplete from '../core/Autocomplete/Autocomplete';
+import Dropdown from '../core/Dropdown/Dropdown';
+import Button from '../core/Button/Button';
+
+import {
+  SUGGESTIONS_MOCK,
+  EMPTY_SUGGESTIONS_MOCK,
+} from '../core/Autocomplete/__mocks__/autocomplete.mocks';
+import { LOCATIONS, CUISINES } from '../../helpers/staticData';
+
+type FinderProps = {
+  className?: string;
+};
+
+const StyledFinder = styled.div`
+  width: 85%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (min-width: 470px) {
+    width: 70%;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 60%;
+  }
+  @media only screen and (min-width: 992px) {
+    width: 58rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+  @media only screen and (min-width: 1250px) {
+    width: 71rem;
+  }
+`;
+
+const StyledAutocomplete = styled(Autocomplete)`
+  width: 100%;
+  margin: 0 0 25px 0;
+  @media only screen and (min-width: 992px) {
+    margin: 0 2% 0 0;
+    width: 39%;
+  }
+  @media only screen and (min-width: 1200px) {
+    width: 44%;
+  }
+`;
+
+const StyledSpacer = styled.span`
+  background-color: ${(props) => props.theme.palette.DARK_SOFT};
+  width: 100%;
+  height: 1px;
+  margin: 0 0 25px 0;
+  max-width: 550px;
+  @media only screen and (min-width: 992px) {
+    width: 1px;
+    height: 90%;
+    margin: 0 2% 0 0;
+  }
+`;
+
+const StyledDropdownsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 22px;
+  &:first-child {
+    margin-right: 2%;
+  }
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+  @media only screen and (min-width: 992px) {
+    width: 47%;
+    justify-content: space-around;
+    margin-bottom: 0;
+    &:first-child {
+      margin-right: 0;
+    }
+  }
+`;
+
+const StyledDropdown = styled(Dropdown)`
+  width: 100%;
+  &:first-of-type {
+    margin-right: 2%;
+    margin-bottom: 15px;
+  }
+  @media only screen and (min-width: 768px) {
+    max-width: 270px;
+    &:first-of-type {
+      margin-bottom: 0;
+    }
+  }
+  @media only screen and (min-width: 992px) {
+    margin-right: 2%;
+    width: 48%;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  width: 100%;
+  margin-bottom: 10px;
+  @media only screen and (min-width: 768px) {
+    width: 300px;
+    margin-bottom: 0;
+  }
+  @media only screen and (min-width: 992px) {
+    width: 10%;
+  }
+`;
+
+const Finder: React.FC<FinderProps> = ({ className }) => {
+  const [suggestions, setSuggestions] = useState(EMPTY_SUGGESTIONS_MOCK);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchSuggestions = useCallback(
+    (search: string) => {
+      setIsLoading(true);
+      setTimeout(() => {
+        console.log('search: ', search);
+        setSuggestions(SUGGESTIONS_MOCK);
+        setIsLoading(false);
+      }, 1000);
+    },
+    [setIsLoading, setSuggestions],
+  );
+
+  const selectSuggestion = (id: string) => {
+    console.log('id: ', id);
+  };
+
+  return (
+    <StyledFinder className={className}>
+      <StyledAutocomplete
+        loaderSrc={`${CDN_URL_STATIC_DIRECTORY}/static/images/loader.svg`}
+        suggestions={suggestions}
+        fetchSuggestions={fetchSuggestions}
+        selectSuggestion={selectSuggestion}
+        loading={isLoading}
+        withSearchIcon={true}
+      />
+      <StyledSpacer />
+      <StyledDropdownsWrapper>
+        <StyledDropdown
+          withIcon="near_me"
+          labelTxt="Select any location"
+          list={LOCATIONS}
+          clearable={true}
+          onSelect={(id: number) => console.log(id)}
+        />
+        <StyledDropdown
+          withIcon="restaurant"
+          labelTxt="Select any cuisine"
+          list={CUISINES}
+          clearable={true}
+          onSelect={(id: number) => console.log(id)}
+        />
+      </StyledDropdownsWrapper>
+      <StyledButton
+        buttonTxt="Search"
+        loading={false}
+        loaderSrc={`${CDN_URL_STATIC_DIRECTORY}/static/images/light_loader.svg`}
+      />
+    </StyledFinder>
+  );
+};
+
+export default Finder;
---
+++ helpers/staticData.tsx
@@ -0,0 +1,258 @@
+import { CDN_URL_STATIC_DIRECTORY } from './utils';
+
+export const LOCATIONS = [
+  {
+    id: 162239,
+    name: 'South City West',
+  },
+  {
+    id: 96201,
+    name: 'Temple Bar',
+  },
+  {
+    id: 95051,
+    name: 'Clondalkin',
+  },
+  {
+    id: 96181,
+    name: 'Swords',
+  },
+  {
+    id: 96191,
+    name: 'Tallaght',
+  },
+  {
+    id: 96201,
+    name: 'North City',
+  },
+  {
+    id: 94881,
+    name: 'Blanchardstown',
+  },
+  {
+    id: 162240,
+    name: 'South City East',
+  },
+  {
+    id: 95311,
+    name: 'Dundrum',
+  },
+  {
+    id: 95341,
+    name: 'Finglas',
+  },
+  {
+    id: 94791,
+    name: 'Ballyfermot',
+  },
+  {
+    id: 96081,
+    name: 'Santry',
+  },
+  {
+    id: 95711,
+    name: 'Lucan',
+  },
+  {
+    id: 94671,
+    name: 'Artane',
+  },
+  {
+    id: 95721,
+    name: 'Malahide',
+  },
+  {
+    id: 95301,
+    name: 'Dun Laoghaire',
+  },
+  {
+    id: 96271,
+    name: 'Walkinstown',
+  },
+  {
+    id: 94871,
+    name: 'Blackrock',
+  },
+  {
+    id: 95851,
+
+    name: 'Palmerstown',
+  },
+  {
+    id: 95981,
+
+    name: 'Rathmines',
+  },
+  {
+    id: 94701,
+    name: 'Balbriggan',
+  },
+  {
+    id: 94761,
+    name: 'Ballybrack',
+  },
+  {
+    id: 96111,
+    name: 'Smithfield',
+  },
+  {
+    id: 95181,
+    name: 'Crumlin',
+  },
+  {
+    id: 94971,
+    name: 'Castleknock',
+  },
+  {
+    id: 95511,
+    name: 'Howth',
+  },
+  {
+    id: 94951,
+    name: 'Cabra',
+  },
+  {
+    id: 96291,
+    name: 'Whitehall',
+  },
+  {
+    id: 95901,
+    name: 'Portmarnock',
+  },
+  {
+    id: 95641,
+    name: 'Kimmage',
+  },
+];
+
+export const CUISINES = [
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/stew.svg`,
+    id: 135,
+    name: 'Irish',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/pizza.svg`,
+    id: 55,
+    name: 'Italian',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/bruschetta.svg`,
+    id: 70,
+    name: 'Mediterranean',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/rice.svg`,
+    id: 3,
+    name: 'Asian',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/healthy_food.svg`,
+    id: 143,
+    name: 'Healthy Food',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/gulab.svg`,
+    id: 148,
+    name: 'Indian',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/taco.svg`,
+    id: 73,
+    name: 'Mexican',
+  },
+  {
+    iconSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/fries.svg`,
+    id: 40,
+    name: 'Fast Food',
+  },
+];
+
+export const HIGHLIGHTED_RESTAURANTS = [
+  {
+    imgSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/cleaver-east.jpg`,
+    id: 1,
+    name: 'Cleaver East Restaurant',
+    description:
+      'Situated in the heart of Ireland’s capital city, on East Essex Street in the popular Temple Bar area, Cleaver East Restaurant offers the finest Irish and European cuisine.',
+    link: 'https://theclarence.ie/cleaver-east/',
+  },
+  {
+    imgSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/tapas-de-lola.jpg`,
+    id: 2,
+    name: 'Tapas de Lola',
+    description:
+      'This restaurant is a love letter to all things Spanish and flavorful. Its owners make regular pilgrimages to Spain for inspiration and pack it all into a lively menu of small plates.',
+    link: 'http://lastapasdelola.com/',
+  },
+  {
+    imgSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/ely-wine.jpg`,
+    id: 3,
+    name: 'Ely Wine Bar',
+    description:
+      'Eric and his wife Michelle are the founders of this clubby place where you can eat and drink well both downstairs in the cozy basement and upstairs in an elegant dining room.',
+    link: 'http://www.elywinebar.ie/',
+  },
+  {
+    imgSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/locks.jpg`,
+    id: 4,
+    name: 'Locks',
+    description:
+      'Locks is a beautiful restaurant with a clever young team in the kitchen. Seasonal menus include dishes like violet artichoke teamed with burrata, broad beans, and fennel.',
+    link: 'https://www.locksrestaurant.ie/',
+  },
+  {
+    imgSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/chapter-one.jpg`,
+    id: 5,
+    name: 'Chapter One',
+    description:
+      'Dublin writers are more closely associated with booze rather than food, but Chapter One is a Michelin-starred restaurant in the basement of the Dublin Writers Museum.',
+    link: 'https://www.chapteronerestaurant.com/',
+  },
+  {
+    imgSrc: `${CDN_URL_STATIC_DIRECTORY}/static/images/rosa-madre.jpg`,
+    id: 6,
+    name: 'Rosa Madre',
+    description:
+      'In the menu-hawking clamor of Temple Bar you might long for a quieter restaurant where you won’t feel like a tourist on a conveyor belt. If so, Rosa Madre is your place.',
+    link: 'http://www.rosamadre.ie/',
+  },
+];
+
+export const HIGHLIGHT_GENERIC_SRC = `${CDN_URL_STATIC_DIRECTORY}/static/images/highlight-generic.png`;
+
+enum FILTER_SORT {
+  cost = 'cost',
+  rating = 'rating',
+}
+enum FILTER_ORDER {
+  asc = 'asc',
+  desc = 'desc',
+}
+
+export const FILTER_DATA = [
+  {
+    text: 'COST - high to low',
+    sort: FILTER_SORT.cost,
+    order: FILTER_ORDER.asc,
+    id: 1,
+  },
+  {
+    text: 'COST - low to high',
+    sort: FILTER_SORT.cost,
+    order: FILTER_ORDER.desc,
+    id: 2,
+  },
+  {
+    text: 'RATING - high to low',
+    sort: FILTER_SORT.rating,
+    order: FILTER_ORDER.asc,
+    id: 3,
+  },
+  {
+    text: 'RATING - low to high',
+    sort: FILTER_SORT.rating,
+    order: FILTER_ORDER.desc,
+    id: 4,
+  },
+];
