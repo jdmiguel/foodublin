0aef2f2e537d70c5c0041d1226a4df47716f19f5
jdmiguel <jaimedmiguel@gmail.com>
2020-07-12 12:56:54 +0100

    add blockTitle component, story and unit test. Update obsolets snapshots

---
+++ components/core/BlockTitle/__tests__/blockTitle.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import BlockTitle from '../BlockTitle';
+
+import { BLOCK_TITLE_TEXT_MOCK } from '../__mocks__/blockTitle.mocks';
+
+import { renderWithTheme } from '../../../../helpers/Theme';
+
+describe('Component: BlockTitle', () => {
+  it('should render', () => {
+    const { container } = render(
+      renderWithTheme(<BlockTitle text={BLOCK_TITLE_TEXT_MOCK} />),
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
