664ce913eb20339f636ee462ef168f726154cc78
jdmiguel <jaimedmiguel@gmail.com>
2020-07-13 14:07:08 +0100

    fix test descriptions

--- helpers/__tests__/utils.spec.tsx
+++ helpers/__tests__/utils.spec.tsx
@@ -57,7 +57,7 @@ describe('getFormattedUrlText', () => {
     expect(getFormattedUrlText(FIRST_DETAIL.name)).toBe(mockedFormattedUrlText);
   });
 
-  it('should be equal to mocked formatted url text if text is composed of more than one word, spaces and symbols', () => {
+  it('should be equal to mocked formatted url text if text is composed of several words, spaces and symbols', () => {
     const mockedFormattedUrlText = 'sophies+the+dean+hotel';
     expect(getFormattedUrlText(FOURTH_DETAIL.name)).toBe(
       mockedFormattedUrlText,
@@ -66,14 +66,14 @@ describe('getFormattedUrlText', () => {
 });
 
 describe('getMapSrc', () => {
-  it('should be equal to mocked mapSrc if name is composed of one word and location is composed of two', () => {
+  it('should be equal to mocked map src if name is composed of one word and location is composed of two', () => {
     const mockedMapSrc = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_EMBED_KEY}&q=boojum-north+city,Dublin&zoom=16`;
     expect(getMapSrc(FIRST_DETAIL.name, FIRST_DETAIL.location)).toBe(
       mockedMapSrc,
     );
   });
 
-  it('should be equal to mocked mapSrc if restaurant name and location are composed of more than one word, spaces and symbols', () => {
+  it('should be equal to mocked map src if restaurant name and location are composed of several words, spaces and symbols', () => {
     const mockedMapSrc = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_EMBED_KEY}&q=sophies+the+dean+hotel-the+dean+hotel+city+centre+south,Dublin&zoom=16`;
     expect(getMapSrc(FOURTH_DETAIL.name, FOURTH_DETAIL.location)).toBe(
       mockedMapSrc,
