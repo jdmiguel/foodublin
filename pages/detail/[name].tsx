145f6ed4874711a4e223847748876b6e764d5a1b
jdmiguel <jaimedmiguel@gmail.com>
2020-07-21 08:13:26 +0100

    update routes

--- pages/detail/[name].tsx
+++ pages/detail/[name].tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NextPageContext } from 'next';
+import { useRouter } from 'next/router';
 
 import DetailPage from '../../components/DetailPage/DetailPage';
 
@@ -8,7 +9,12 @@ import DetailPage from '../../components/DetailPage/DetailPage';
 };*/
 
 //const Detail = ({ id }: DetailProps) => <DetailPage />;
-const Detail = () => <DetailPage />;
+const Detail = () => {
+  const router = useRouter();
+  console.log(router.query);
+
+  return <DetailPage />;
+};
 
 Detail.getInitialProps = async ({ query }: NextPageContext) => {
   const { id } = query;
--- pages/search.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import SearchPage from '../components/SearchPage/SearchPage';
-
-const search = () => <SearchPage />;
-
-export default search;
