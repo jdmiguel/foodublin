e94241ee19aa3596b5e26678905a5f3ecd688b79
jdmiguel <jaimedmiguel@gmail.com>
2020-09-27 09:31:10 +0100

    update HomePage to avoid warning of differences between server & client

--- components/pages/HomePage/HomePage.tsx
+++ components/pages/HomePage/HomePage.tsx
@@ -39,30 +39,36 @@ const StyledHighlightWrapper = styled.div`
   }
 `;
 
-const HomePage: React.FC<HomePageProps> = ({ isLoading, clickHighlight }) => (
-  <Layout isExtendedHeader={true} isExtendedFooter={true}>
-    <FullLoader isShowed={isLoading}>
+const HomePage: React.FC<HomePageProps> = ({ isLoading, clickHighlight }) => {
+  return isLoading ? (
+    <FullLoader>
       <Loader text={DEFAULT_TEXT_LOADING} />
     </FullLoader>
-    <StyledHighlights className="grid-container">
-      <Title text="Featured restaurants" />
-      <StyledHighlightWrapper className="grid-x grid-margin-x grid-margin-y">
-        {HIGHLIGHTED_RESTAURANTS.map((restaurant) => (
-          <div key={restaurant.id} className="cell small-12 medium-6 large-4">
-            <Link href={restaurant.route} as={restaurant.asRoute}>
-              <Card
-                imgSrc={restaurant.imgSrc}
-                title={restaurant.title}
-                content={restaurant.content}
-                type={CardType.HIGHLIGHT}
-                onClick={() => clickHighlight(restaurant.id)}
-              />
-            </Link>
-          </div>
-        ))}
-      </StyledHighlightWrapper>
-    </StyledHighlights>
-  </Layout>
-);
+  ) : (
+    <Layout isExtendedHeader={true} isExtendedFooter={true}>
+      <FullLoader isShowed={isLoading}>
+        <Loader text={DEFAULT_TEXT_LOADING} />
+      </FullLoader>
+      <StyledHighlights className="grid-container">
+        <Title text="Featured restaurants" />
+        <StyledHighlightWrapper className="grid-x grid-margin-x grid-margin-y">
+          {HIGHLIGHTED_RESTAURANTS.map((restaurant) => (
+            <div key={restaurant.id} className="cell small-12 medium-6 large-4">
+              <Link href={restaurant.route} as={restaurant.asRoute}>
+                <Card
+                  imgSrc={restaurant.imgSrc}
+                  title={restaurant.title}
+                  content={restaurant.content}
+                  type={CardType.HIGHLIGHT}
+                  onClick={() => clickHighlight(restaurant.id)}
+                />
+              </Link>
+            </div>
+          ))}
+        </StyledHighlightWrapper>
+      </StyledHighlights>
+    </Layout>
+  );
+};
 
 export default HomePage;
