e960f17e6d88568d5bb6c4d664869482b4f0bd08
jdmiguel <jaimedmiguel@gmail.com>
2020-10-09 08:07:20 +0100

    forwardRef remove from Card

--- components/core/Card/Card.tsx
+++ components/core/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, forwardRef } from 'react';
+import React, { ReactNode } from 'react';
 
 import {
   StyledCard,
@@ -23,47 +23,45 @@ type CardProps = {
   onClick?: () => void;
 };
 
-export const Card = forwardRef<HTMLAnchorElement, CardProps>(
-  (
-    { imgSrc, title, content, onClick, type = CardType.STANDART },
-    forwardedRef,
-  ) => (
-    <StyledCard
-      ref={forwardedRef}
-      className={type === CardType.SUGGESTION ? '' : 'paper'}
-      onClick={onClick && onClick}
+export const Card: React.FC<CardProps> = ({
+  imgSrc,
+  title,
+  content,
+  onClick,
+  type = CardType.STANDART,
+}) => (
+  <StyledCard
+    className={type === CardType.SUGGESTION ? '' : 'paper'}
+    onClick={onClick && onClick}
+    type={type}
+    data-testid="card"
+  >
+    <StyledImage
+      src={imgSrc}
+      alt={title}
       type={type}
-      data-testid="card"
-    >
-      <StyledImage
-        src={imgSrc}
-        alt={title}
-        type={type}
-        placeholder={({ imageProps, ref }) => (
-          <div ref={ref} className="LazyImage-Placeholder">
-            <StyledGenericThumb
-              src={
-                type === CardType.HIGHLIGHT
-                  ? HIGHLIGHT_GENERIC_SRC
-                  : THUMB_GENERIC_SRC
-              }
-              alt={imageProps.alt}
-              type={type}
-            />
-          </div>
-        )}
-        actual={({ imageProps }) => (
-          <div className="LazyImage-Actual">
-            <img {...imageProps} alt={title} />
-          </div>
-        )}
-      />
-      <StyledText type={type}>
-        <StyledTitle type={type}>{title}</StyledTitle>
-        <StyledContent type={type}>{content}</StyledContent>
-      </StyledText>
-    </StyledCard>
-  ),
+      placeholder={({ imageProps, ref }) => (
+        <div ref={ref} className="LazyImage-Placeholder">
+          <StyledGenericThumb
+            src={
+              type === CardType.HIGHLIGHT
+                ? HIGHLIGHT_GENERIC_SRC
+                : THUMB_GENERIC_SRC
+            }
+            alt={imageProps.alt}
+            type={type}
+          />
+        </div>
+      )}
+      actual={({ imageProps }) => (
+        <div className="LazyImage-Actual">
+          <img {...imageProps} alt={title} />
+        </div>
+      )}
+    />
+    <StyledText type={type}>
+      <StyledTitle type={type}>{title}</StyledTitle>
+      <StyledContent type={type}>{content}</StyledContent>
+    </StyledText>
+  </StyledCard>
 );
-
-Card.displayName = 'Card';
