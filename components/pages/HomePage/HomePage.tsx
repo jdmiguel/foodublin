3b3f56cf6d7af48f8688d5a303ac91becd800a93
jdmiguel <jaimedmiguel@gmail.com>
2021-01-03 15:03:11 +0000

    test

--- components/pages/HomePage/HomePage.tsx
+++ components/pages/HomePage/HomePage.tsx
@@ -8,7 +8,7 @@ import { Card } from '../../core/Card/Card';
 import { StyledHighlights, StyledHighlightWrapper } from './styles';
 
 import { CardType } from '../../core/types';
-import { HighlightRestaurant } from '../types';
+//import { HighlightRestaurant } from '../types';
 
 type HomePageProps = {
   highlights: HighlightRestaurant[];
