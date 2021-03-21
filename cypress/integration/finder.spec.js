aaefc6d99b208f5f23450a8593e593aa689b2b9f
jdmiguel <jaimedmiguel@gmail.com>
2021-03-21 11:28:55 +0000

    split integration test

--- cypress/integration/finder.spec.js
+++ cypress/integration/finder.spec.js
@@ -1,6 +1,6 @@
-import { HIGHLIGHTED_RESTAURANTS, LOCATIONS, CUISINES } from '@/store/statics';
+import { LOCATIONS, CUISINES } from '@/store/statics';
 
-describe('HomePage', () => {
+describe('Finder', () => {
   beforeEach(() => {
     cy.visit('/');
 
@@ -57,35 +57,35 @@ describe('HomePage', () => {
       cy.get('@listBox').find('[role="option"]').eq(1).as('secondSuggestion');
       cy.get('@listBox').find('[role="option"]').eq(2).as('thirdSuggestion');
 
-      cy.get('@firstSuggestion')
-        .find('h4')
-        .should('have.text', 'The Bank on College Green');
+      cy.get('@firstSuggestion').find('h4').should('have.text', 'Tutti Frutti');
 
       cy.get('@firstSuggestion')
         .find('p')
-        .should('have.text', 'South City West');
+        .should('have.text', 'Town Centre Mall, Swords');
 
       cy.get('@firstSuggestion')
         .find('img')
-        .should('have.attr', 'alt', 'The Bank on College Green')
+        .should('have.attr', 'alt', 'Tutti Frutti')
         .should(
           'have.attr',
           'src',
-          'https://b.zmtcdn.com/data/res_imagery/16522174_RESTAURANT_246ce7cd6fcdb531a0806c042f032eba.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+          'https://b.zmtcdn.com/data/res_imagery/9100465_RESTAURANT_d9cb91300a2dde4403ff422884013f2c_c.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
         );
 
-      cy.get('@secondSuggestion')
-        .find('h4')
-        .should('have.text', "Coletti's Take Away");
+      cy.get('@secondSuggestion').find('h4').should('have.text', 'Cornucopia');
 
       cy.get('@secondSuggestion')
         .find('p')
-        .should('have.text', 'Firhouse Shopping Centre, Firhouse');
+        .should('have.text', 'South City West');
 
       cy.get('@secondSuggestion')
         .find('img')
-        .should('have.attr', 'alt', "Coletti's Take Away")
-        .should('have.attr', 'src', '/images/generic-thumb.png');
+        .should('have.attr', 'alt', 'Cornucopia')
+        .should(
+          'have.attr',
+          'src',
+          'https://b.zmtcdn.com/data/res_imagery/16521303_RESTAURANT_c6e5a59d49548f7ccfa17cf9d123f74a.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+        );
 
       cy.get('@thirdSuggestion')
         .find('h4')
@@ -137,6 +137,31 @@ describe('HomePage', () => {
       cy.url().should('equal', 'http://localhost:3000/search/dublin/any-food');
     });
 
+    describe('after selecting a location and a cuisine', () => {
+      it('should navigate to search page with the correct location and cuisine paths', () => {
+        cy.get('@locationsDropdown').click();
+
+        cy.get('@locationsDropdown')
+          .find('[role="option"]')
+          .contains('Temple Bar')
+          .click();
+
+        cy.get('@cuisinesDropdown').click();
+
+        cy.get('@cuisinesDropdown')
+          .find('[role="option"]')
+          .contains('Asian')
+          .click();
+
+        cy.get('@searchButton').click();
+
+        cy.url().should(
+          'equal',
+          'http://localhost:3000/search/temple-bar/asian',
+        );
+      });
+    });
+
     describe('after selecting a location', () => {
       it('should navigate to search page with the correct location path', () => {
         cy.get('@locationsDropdown').click();
@@ -169,76 +194,5 @@ describe('HomePage', () => {
         cy.url().should('equal', 'http://localhost:3000/search/dublin/asian');
       });
     });
-
-    describe('after selecting a location and a cuisine', () => {
-      it('should navigate to search page with the correct location and cuisine paths', () => {
-        cy.get('@locationsDropdown').click();
-
-        cy.get('@locationsDropdown')
-          .find('[role="option"]')
-          .contains('Temple Bar')
-          .click();
-
-        cy.get('@cuisinesDropdown').click();
-
-        cy.get('@cuisinesDropdown')
-          .find('[role="option"]')
-          .contains('Asian')
-          .click();
-
-        cy.get('@searchButton').click();
-
-        cy.url().should(
-          'equal',
-          'http://localhost:3000/search/temple-bar/asian',
-        );
-      });
-    });
-  });
-
-  it('should display the highlights title', () => {
-    cy.get('[data-testid="highlights"]')
-      .find('h3')
-      .should('have.text', 'Featured restaurants');
-  });
-
-  it('should display the highlights cards', () => {
-    cy.get('footer').scrollIntoView({ duration: 1000, easing: 'linear' });
-
-    cy.get('[data-testid="card"]').should('have.length', 6);
-
-    cy.wrap(HIGHLIGHTED_RESTAURANTS).each((highlight, index) => {
-      cy.get('[data-testid="card"]').eq(index).as('highlight');
-
-      cy.get('@highlight')
-        .find('img')
-        .should('have.attr', 'alt', highlight.title)
-        .should('have.attr', 'src', highlight.featuredSrc);
-
-      cy.get('@highlight').find('h4').should('have.text', highlight.title);
-
-      cy.get('@highlight').find('p').should('have.text', highlight.content);
-    });
-  });
-
-  describe('When clicking a highlight card', () => {
-    it('should navigate to the correct detail page', () => {
-      cy.get('[data-testid="card"]').eq(0).click();
-
-      cy.url().should(
-        'equal',
-        'http://localhost:3000/detail/9100233/cleaver-east',
-      );
-    });
-  });
-
-  it('should display the footer with the correct Breadcrumbs', () => {
-    cy.get('footer').should('have.length', 1);
-
-    cy.get('[data-testid="breadcrumbs"]')
-      .should('have.length', 1)
-      .find('div')
-      .should('have.length', 1)
-      .should('have.text', 'Home');
   });
 });
---
+++ mocks/data/default-restaurants.js
@@ -0,0 +1,29 @@
+export const defaultRestaurants = [
+  {
+    asRoute: '/detail/9100913/elephant-castle',
+    content: 'Temple Bar',
+    id: '9100913',
+    imgSrc:
+      'https://b.zmtcdn.com/data/res_imagery/9100913_RESTAURANT_914e43ebf23f1d46e8895bff65c86270_c.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+    route: '/detail/[id]/[name]',
+    title: 'Elephant & Castle',
+  },
+  {
+    asRoute: '/detail/9101503/bunsen',
+    content: 'Temple Bar',
+    id: '9100913',
+    imgSrc:
+      'https://b.zmtcdn.com/data/res_imagery/16518534_CHAIN_2496df898a20e6848015740554b58169_c.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+    route: '/detail/[id]/[name]',
+    title: 'Bunsen',
+  },
+  {
+    asRoute: '/detail/9101725/klaw',
+    content: 'Temple Bar',
+    id: '9100913',
+    imgSrc:
+      'https://b.zmtcdn.com/data/res_imagery/9101725_RESTAURANT_2263f294ab88470066d710def1c17b3e_c.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
+    route: '/detail/[id]/[name]',
+    title: 'Klaw',
+  },
+];
