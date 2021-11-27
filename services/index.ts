21d1bcd0d3ae774fa386911aedcadd8c93699d1a
Jaime <jaimedmiguel@gmail.com>
2021-11-27 13:39:40 +0000

    update env var name

--- services/index.ts
+++ services/index.ts
@@ -47,7 +47,7 @@ export const getRestaurants = async (
     const response = await axios(`${BASE_API}search`, {
       method: 'GET',
       headers: {
-        'user-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+        'user-key': `${process.env.NEXT_PUBLIC_ZOMATO_API_KEY}`,
         'content-type': 'application/json',
       },
       params: currentParams,
@@ -70,7 +70,7 @@ export const getRestaurant = async (
     const response = await axios(`${BASE_API}restaurant?res_id=${res_id}`, {
       method: 'GET',
       headers: {
-        'user-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+        'user-key': `${process.env.NEXT_PUBLIC_ZOMATO_API_KEY}`,
         'content-type': 'application/json',
       },
     });
@@ -88,7 +88,7 @@ export const getReviews = async (
     const response = await axios(`${BASE_API}reviews?res_id=${res_id}`, {
       method: 'GET',
       headers: {
-        'user-key': `${process.env.NEXT_PUBLIC_API_KEY}`,
+        'user-key': `${process.env.NEXT_PUBLIC_ZOMATO_API_KEY}`,
         'content-type': 'application/json',
       },
     });
